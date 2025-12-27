f1298e838913915d038c4ed201e01e42d3765b75
yakhya <yakhyatimurkaev2006@gmail.com>
2025-12-27 04:09:50 +0100

    ajout de la variable qui va renvoyé le choix de l'utilisateur

--- script/config.js
+++ script/config.js
@@ -1,8 +1,8 @@
 const listeMots = ["Cachalot", "Pétunia", "Serviette"]
 const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
-let baliseZoneProposition = document.getElementById("zoneProposition")
 let txtUtilisateur = document.getElementById("réponse")
 let validation = document.getElementById("valider")
-let proposition = document.querySelector("#zoneProposition")
+let proposition = document.querySelector(".proposition input")
 let score = 0
 let scoreUtilisateur = document.querySelector("#score")
+let choixUtilisateur = document.querySelectorAll(".choixUtilisateur input")
